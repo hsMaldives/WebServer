dd150d4a397eda37e2149b39e86c1e33647d1fd1
gim224 <c233c@naver.com>
2017-05-02 14:37:07 +0900

    위치를 받아 매장 리스트를 전달

--- src/main/java/kr/ac/hansung/maldives/web/controller/ApiContorller.java
+++ src/main/java/kr/ac/hansung/maldives/web/controller/ApiContorller.java
@@ -1,13 +1,20 @@
 package kr.ac.hansung.maldives.web.controller;
 
 import java.security.Principal;
+import java.util.Comparator;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.ac.hansung.maldives.web.model.Location;
 import kr.ac.hansung.maldives.web.model.LocationAndRating;
+import kr.ac.hansung.maldives.web.model.Store;
+import kr.ac.hansung.maldives.web.service.StoreService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -16,6 +23,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/rating")
 public class ApiContorller {
 	
+	@Autowired
+	private StoreService storeService;
+	
 	@RequestMapping(value="/test", method=RequestMethod.POST)
 	public LocationAndRating test(@RequestBody LocationAndRating locationAndRating, Principal principal){
 		System.out.println(locationAndRating);
@@ -23,4 +33,40 @@ public class ApiContorller {
 		
 		return locationAndRating;
 	}
+	
+	
+	
+	@RequestMapping(value="/locationInfo", method=RequestMethod.POST)
+	@ResponseBody
+	public List<Store> location(@RequestBody Location location){
+		System.out.println(location);
+		log.info("[위치 정보] location: {}", location);
+		
+		
+		////////////가장 가까운 매장 정보
+		final double LATI = location.getLati();
+		final double LONGI = location.getLongi();
+		final double FOR_RADIUS=0.0045; //반경 약 500m		//0.009; //반경 약 1000m		//0.0135;//반경 약 1500m
+		
+		List<Store> stores = storeService.getStoresByBound(location.getLati()-FOR_RADIUS, location.getLati()+FOR_RADIUS, location.getLongi()-FOR_RADIUS, location.getLongi()+FOR_RADIUS);
+		
+		class MyComparator implements Comparator<Store>{
+
+			@Override
+			public int compare(Store o1, Store o2) {								
+				return (int) ((Math.pow(o1.getLatitude()-LATI,2)+Math.pow(o1.getLongitude()-LONGI, 2))- (Math.pow(o2.getLatitude()-LATI,2)+Math.pow(o2.getLongitude()-LONGI, 2)));			
+			}
+			
+		}
+		
+		stores.sort(new MyComparator());
+		
+		return stores;
+	}
+	
+	
+	
+	
+	
+	
 }
---
+++ src/main/java/kr/ac/hansung/maldives/web/model/Location.java
@@ -0,0 +1,9 @@
+package kr.ac.hansung.maldives.web.model;
+
+import lombok.Data;
+
+@Data
+public class Location {
+	private double lati;
+	private double longi;
+}
