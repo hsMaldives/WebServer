15cb3ae201914e7026113ff0f5047d88353347ed
KGT <gim224@github.com>
2017-05-31 01:33:05 +0900

    Point add,use,spended 완료.

--- src/main/java/kr/ac/hansung/maldives/web/dao/PointRepository.java
+++ src/main/java/kr/ac/hansung/maldives/web/dao/PointRepository.java
@@ -6,10 +6,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.ac.hansung.maldives.web.model.PointLog;
+import kr.ac.hansung.maldives.web.model.PointLog.PointType;
 
 public interface PointRepository extends JpaRepository<PointLog, Long>{
 	
 	//수정요망
 	public List<PointLog> findByUserUserIdxAndTimestampBetween(Long userIdx, LocalDateTime beginTime, LocalDateTime endTime);
 	
+	
+	public List<PointLog> findByUserUserIdxAndPointType(Long userIdx,PointType pointType);
+	
+	
+	//PointType.SPEND
+	//@Query("SELECT SUM(pl.accPoint) FROM PointLog pl WHERE pl.user_idx=:userIdx AND pl.pointType=:pointType")
 }
