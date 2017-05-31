7859545c897afc71bd7b83d36a6842b1c14b2c6b
MingyuBae <a3811955@gmail.com>
2017-05-31 15:59:38 +0900

    추천 서비스에서 이미 평가한 장소는 다시 안뜨도록 수정

--- src/main/java/kr/ac/hansung/maldives/web/service/RecommendService.java
+++ src/main/java/kr/ac/hansung/maldives/web/service/RecommendService.java
@@ -1,30 +1,19 @@
 package kr.ac.hansung.maldives.web.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.rosuda.REngine.REXPMismatchException;
-import org.rosuda.REngine.Rserve.RConnection;
-import org.rosuda.REngine.Rserve.RserveException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.ac.hansung.maldives.web.dao2.IbcfMapper;
 import kr.ac.hansung.maldives.web.dao2.UbcfMapper;
-import kr.ac.hansung.maldives.web.model.Evaluation;
 import kr.ac.hansung.maldives.web.model.Store;
 
 @Transactional
 @Service
 public class RecommendService {
-
-	@Autowired
-	private StoreService storeService;
-
-	@Autowired
-	private EvaluationService evaluationService;
 
 	@Autowired
 	private IbcfMapper ibcfMapper;
@@ -33,34 +22,11 @@ public class RecommendService {
 	private UbcfMapper ubcfMapper;
 
 	public List<Store> getRecommendIBStore(long user_idx) {
-
-		List<Store> mine = new ArrayList<Store>();
-		List<Evaluation> e = evaluationService.findByUser_idxAndNotEmptyEvaluation(user_idx);
-		for (int i = 0; i < e.size(); i++) {
-			Store store = e.get(i).getPosition().getStore();
-			if (!mine.contains(store)) {
-				mine.add(store);
-			}
-		}
-		e.clear();
-		List<Store> ls = new ArrayList<Store>();
 		int colNum[] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		
-		ls = ibcfMapper.selectStoresByUserIdx(user_idx, colNum, 2.0f);
-		if (ls.size() != 0) {
-			for (int j = 0; j < ls.size();) {
-				if (mine.contains(ls.get(j))) {
-					ls.remove(ls.get(j));
-				} else {
-					j++;
-				}
-			}
-
-		}
+		List<Store> ls = ibcfMapper.selectStoresByUserIdx(user_idx, colNum, 2.0f);
 		return ls;
 	}
-
-	
 
 	public List<Store> getRecommendUBStore(long user_idx) {
 		int colNum[] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
