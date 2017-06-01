8f3bfc1136d12f28c2e2cfd54f2dcbb3a63c20aa
MingyuBae <a3811955@gmail.com>
2017-06-02 06:59:41 +0900

    lucy-xss를 이용해 XSS 공격 차단

--- src/main/java/kr/ac/hansung/maldives/web/WhereYouApplication.java
+++ src/main/java/kr/ac/hansung/maldives/web/WhereYouApplication.java
@@ -3,7 +3,10 @@ package kr.ac.hansung.maldives.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,5 +14,13 @@ public class WhereYouApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhereYouApplication.class, args);
+	}
+	
+	/* lucy-xss filter */
+	@Bean
+	public XssEscapeServletFilter xssEscapeServletFilter(){
+		XssEscapeServletFilter xssEscapeServletFilter = new XssEscapeServletFilter();
+		
+		return xssEscapeServletFilter;
 	}
 }
